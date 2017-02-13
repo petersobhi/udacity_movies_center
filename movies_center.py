d7d1a23c9dd89cbf5efdca405d7eaa9743c441e0
peter <peterbsobhi@gmail.com>
2017-02-13 18:25:18 +0200

    improve code to meet Pep 8 standards

--- movies_center.py
+++ movies_center.py
@@ -1,13 +1,16 @@
 import movie
 import fresh_tomatoes
-import webbrowser
 
+
+# assign movies data to their objects
 the_godfather = movie.Movie("The Godfather",
-                            "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Godfather_ver1.jpg/220px-Godfather_ver1.jpg",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Godfather_ver1.jpg/220px"
+                            "-Godfather_ver1.jpg",
                             "https://www.youtube.com/watch?v=sY1S34973zA")
 
 fight_club = movie.Movie("Fight Club",
-                         "https://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Fight_Club_poster.jpg/220px-Fight_Club_poster.jpg",
+                         "https://upload.wikimedia.org/wikipedia/en/thumb/f/fc/Fight_Club_poster.jpg/220px"
+                         "-Fight_Club_poster.jpg",
                          "https://www.youtube.com/watch?v=SUXWAEX2jlg")
 
 the_green_mile = movie.Movie("The Green Mile",
@@ -15,28 +18,33 @@ the_green_mile = movie.Movie("The Green Mile",
                              "https://www.youtube.com/watch?v=ctRK-4Vt7dA")
 
 the_shawshank_redemption = movie.Movie("The Shawshank Redemption",
-                                       "https://upload.wikimedia.org/wikipedia/en/thumb/8/81/ShawshankRedemptionMoviePoster.jpg/220px-ShawshankRedemptionMoviePoster.jpg",
-                                        "https://www.youtube.com/watch?v=6hB3S9bIaco")
+                                       "https://upload.wikimedia.org/wikipedia/en/thumb/8/81"
+                                       "/ShawshankRedemptionMoviePoster.jpg/220px-ShawshankRedemptionMoviePoster.jpg",
+                                       "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
 three_idiots = movie.Movie("3 Idiots",
-                           "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/3_idiots_poster.jpg/220px-3_idiots_poster.jpg",
+                           "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/3_idiots_poster.jpg/220px"
+                           "-3_idiots_poster.jpg",
                            "https://www.youtube.com/watch?v=K0eDlFX9GMc")
 
-
 the_lion_king = movie.Movie("The Lion King",
-                            "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg",
+                            "https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px"
+                            "-The_Lion_King_poster.jpg",
                             "https://www.youtube.com/watch?v=4sj1MT05lAA")
 
 seven = movie.Movie("Se7en",
-                    "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Seven_%28movie%29_poster.jpg/220px-Seven_%28movie%29_poster.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Seven_%28movie%29_poster.jpg/220px-Seven_"
+                    "%28movie%29_poster.jpg",
                     "https://www.youtube.com/watch?v=znmZoVkCjpI")
 
 saving_private_ryan = movie.Movie("Saving Private Ryan",
-                                  "https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/Saving_Private_Ryan_poster.jpg/220px-Saving_Private_Ryan_poster.jpg",
+                                  "https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/Saving_Private_Ryan_poster"
+                                  ".jpg/220px-Saving_Private_Ryan_poster.jpg",
                                   "https://www.youtube.com/watch?v=zwhP5b4tD6g")
 
+# the movies list to be shown in the web page
+movies_list = [the_godfather, fight_club, the_green_mile, the_shawshank_redemption,
+               three_idiots, the_lion_king, seven, saving_private_ryan]
 
-movies_list = [the_godfather, fight_club,the_green_mile,the_shawshank_redemption,three_idiots,the_lion_king,seven,saving_private_ryan]
-
-
+# this function generates the web page and opens it in the web browser
 fresh_tomatoes.open_movies_page(movies_list)
